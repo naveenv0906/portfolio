e5dc2afa607b137ad518dbdff4c5a441c7026ddd
naveenv0906 <naveenv0906@gmail.com>
2024-09-05 22:03:14 +0530

    resume changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,8 +26,9 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, Spring Boot and MongoDB. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, Node.js, Express.js and MongoDB. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/naveenv0906/Chatify"
+              demoLink="https://chatify24.netlify.app/"  
             />
           </Col>
 
@@ -36,8 +37,9 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="Cart Nest"
-              description="A dynamic ecommerce platform built with React.js, Spring Boot, and MongoDB, offering secure user authentication and robust product management. The responsive frontend features a visually appealing product display and an animated dashboard for real-time analytics. The platform is optimized for seamless user experience across all devices."
-              ghLink="https://github.com/naveenv0906/Dyno-E-Commerce"
+              description="A dynamic ecommerce platform built with React.js, Node.js, Express.js and MongoDB, offering secure user authentication and robust product management. The responsive frontend features a visually appealing product display and an animated dashboard for real-time analytics. The platform is optimized for seamless user experience across all devices."
+              ghLink="https://github.com/naveenv0906/Cart-Nest"
+              demoLink="https://cartnest24.netlify.app/"
             />
           </Col>
 
@@ -46,7 +48,7 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="Todo Stream"
-              description="A powerful TodoList application built with React.js, Spring Boot, and MySQL, featuring user-specific task management and secure authentication. The responsive frontend ensures a seamless experience across all devices, while the backend efficiently handles task creation, updates, and deletion. The platform is designed for scalability and user-centric functionality."
+              description="A powerful TodoList application built with React.js, Node.js, Express.js and MongoDB, featuring user-specific task management and secure authentication. The responsive frontend ensures a seamless experience across all devices, while the backend efficiently handles task creation, updates, and deletion. The platform is designed for scalability and user-centric functionality."
               ghLink="https://github.com/naveenv0906/TO-DO-LIST.git"
               demoLink="https://todo-stream.netlify.app/"              
             />
@@ -58,7 +60,7 @@ function Projects() {
               isBlog={false}
               title="Plant AI"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/naveenv0906/Plant_AI"
+              ghLink="https://github.com/naveenv0906/"
             />
           </Col>
 
@@ -79,7 +81,7 @@ function Projects() {
               title="Face Recognition and Emotion Detection"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/naveenv0906/Face_And_Emotion_Detection" 
+              ghLink="https://github.com/naveenv0906/" 
             />
           </Col>
         </Row>
